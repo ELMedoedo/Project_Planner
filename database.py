0d2be270cc006522ffee707cec1c2596ed124a67
arinaqwc <arinaalekseeva2008@mail.ru>
2025-03-28 21:06:03 +0300

    Дополнила старый код

--- database.py
+++ database.py
@@ -1,12 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session
 from config import SQLALCHEMY_DATABASE_URI
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 engine=create_engine(SQLALCHEMY_DATABASE_URI)
 
-db_session=scoped_session(sessionmaler(bind=engine))
+db_session=scoped_session(sessionmaker(bind=engine))
 
-db=SQLALchemy
+db=SQLAlchemy(app)
 
-class Basa(DeclarativeBase):
-    pass
+class Base(db.Model):
+    __abstract__ = True
+
