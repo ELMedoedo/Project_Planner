8911b675e2dbe6c049ccb3dd50c5efc0c7ca7a3c
arinaqwc <arinaalekseeva2008@mail.ru>
2025-04-02 13:01:02 +0300

    Маленькие корректировки

--- database.py
+++ database.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from server.py import app
 from config import SQLALCHEMY_DATABASE_URI
 
-app = Flask(__name__)
+
+app = app
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 engine=create_engine(SQLALCHEMY_DATABASE_URI)
 
